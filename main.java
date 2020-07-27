5d1303d97f15849030ab305a3fe274ccd7d5c17c
unknown <rpramodh811@gmail.com>
2020-07-27 16:58:35 +0530

    main

---
+++ main.java
@@ -0,0 +1,12 @@
+package git_pkg;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		System.out.println("pizza");
+		System.out.println("burger");
+		System.out.println("franky");
+	}
+
+}
